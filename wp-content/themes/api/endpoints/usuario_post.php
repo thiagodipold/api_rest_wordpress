82065b7e406cc08b2a29a072874193a737771ef3
Thiago Dipold <thiago.cavalcanti@iteris.com.br>
2021-04-19 22:32:20 -0300

    endpoint de criação de usuário e de campos personalizados a ele

--- wp-content/themes/api/endpoints/usuario_post.php
+++ wp-content/themes/api/endpoints/usuario_post.php
@@ -2,10 +2,39 @@
 
 function api_usuario_post($request){
 
-    $response = array(
-        'nome' => 'Andre',
-        'email' => 'thiagodipcavalcanti@gmail.com'
-    );
+    $email = sanitize_email($request['email']);
+    $nome = sanitize_text_field($request['nome']);
+    $senha = $request['senha'];
+    $rua = sanitize_text_field($request['rua']);
+    $cep = sanitize_text_field($request['cep']);
+    $numero = sanitize_text_field($request['numero']);
+    $bairro = sanitize_text_field($request['bairro']);
+    $cidade = sanitize_text_field($request['cidade']);
+    $estado = sanitize_text_field($request['estado']);
+
+    $user_exists = username_exists($email);
+    $email_exists = email_exists($email);
+
+    if(!$user_exists && !$email_exists && $email && $senha) {
+        $user_id = wp_create_user($email, $senha, $email);
+        $response = array(
+            'ID' => $user_id,
+            'display_name' => $nome,
+            'first_name' => $nome,
+            'role' => 'subscriber',
+        );
+
+        wp_update_user($response);
+
+        update_user_meta($user_id, 'cep', $cep);
+        update_user_meta($user_id, 'rua', $rua);
+        update_user_meta($user_id, 'numero', $numero);
+        update_user_meta($user_id, 'bairro', $bairro);
+        update_user_meta($user_id, 'cidade', $cidade);
+        update_user_meta($user_id, 'estado', $estado);
+    } else {
+        $response = new WP_Error('email', 'E-mail já cadastrado', array('status' => 403));
+    }
 
     return rest_ensure_response($response);
 }
