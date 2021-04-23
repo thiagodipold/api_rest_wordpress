587ad0b13670d4fc3ff79884669a2e88d9f63c5e
Thiago Dipold Cavalcanti - TCV <thiago.cavalcanti@iteris.com.br>
2021-04-23 00:00:48 -0300

    GET de produtos com paginação e queryparams

--- wp-content/themes/api/endpoints/produto_get.php
+++ wp-content/themes/api/endpoints/produto_get.php
@@ -63,17 +63,47 @@ function api_produtos_get($request) {
     $_limit = sanitize_text_field($request['_limit']) ?: 9;
     $usuario_id = sanitize_text_field($request['usuario_id']);
 
+    $usuario_id_query = null;
+
+    if($usuario_id) {
+        $usuario_id_query = array(
+            'key' => 'usuario_id',
+            'value' => $usuario_id,
+            'compare' => '=',
+        );
+    }
+
+    $vendido = array(
+        'key' => 'vendido',
+        'value' => 'false',
+        'compare' => '='
+    );
+
     $query = array(
         'post_type' => 'produto',
         'posts_per_page' => $_limit,
         'paged' => $_page,
         's' => $q,
+        'meta_query' => array(
+            $usuario_id_query,
+            $vendido,
+        ),
     );
 
     $loop = new WP_Query($query);
     $posts = $loop->posts;
+    $total = $loop->found_posts;
 
-    return rest_ensure_response($posts);
+    $produtos = array();
+    foreach($posts as $key => $value) {
+        $produtos[] = produto_schema($value->post_name);
+    }
+
+    $response = rest_ensure_response($produtos);
+
+    $response->header('X-Total-Count', $total);
+
+    return $response;
 }
 
 function registrar_api_produtos_get() {
